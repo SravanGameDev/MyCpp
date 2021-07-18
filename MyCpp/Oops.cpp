10d765a7fc328b40125c1dfb07329cb36ea8e33e
Sravan Kumar K <sravan.kumar1691@gmail.com>
2021-07-18 20:40:17 +0530

    Encapsulation
    Data Hiding

--- MyCpp/Oops.cpp
+++ MyCpp/Oops.cpp
@@ -2,34 +2,100 @@
 #include<list>
 using namespace std;
 
+#pragma region Constructor
+
 class GameGenre
 {
 public:
 	string GameName;
 	string GameDeveloper;
-	int GameCopies=0;
 	list<string> GameSoldCopies;
 
-	GameGenre(string name)
+	GameGenre(string name, string developer)
 	{
 		GameName = name;
+		GameDeveloper = developer;
+	}
+
+	void GetInfo()
+	{
+		cout << "Game Name: " << GameName << endl;
+		cout << "Number of copies sold" << endl;
+		for (string title : GameSoldCopies)
+		{
+			cout << title << endl;
+		}
+		cout<<endl;
 	}
 };
 
+
+void GameGenreData()
+{
+	GameGenre action("Read Dead Redemption", "Rockstar North");
+	action.GameSoldCopies.push_back("Hundered");
+	action.GameSoldCopies.push_back("after a year Million");
+	action.GameSoldCopies.push_back("after a decade Billion");
+
+	GameGenre OpenWorld("GTA V", "Rockstar Games");
+	OpenWorld.GameSoldCopies.push_back("Hundered");
+	OpenWorld.GameSoldCopies.push_back("Close a million");
+
+	action.GetInfo();
+	OpenWorld.GetInfo();
+}
+
+#pragma endregion
+
+
+#pragma region Encapsulation
+
+class PlayerData
+{
+private:
+	string Name;
+	int Kills;
+	int Deaths;
+	list<string> rewards;
+	
+public:
+	PlayerData(string name, int kill, int death)
+	{
+		Name = name;
+		Kills = kill;
+		Deaths = death;
+	}
+
+	void GetInfo()
+	{
+		cout << "Player Name: " << Name<<endl;
+		cout << "Kills: " << Kills<<endl;
+		cout << "Deaths: " << Deaths<<endl;
+		cout << "Achivements:" << endl;
+		for (string playerRewards : rewards)
+		{
+			cout << playerRewards<<endl;
+		}
+		cout << endl;
+	}
+
+	void AddRewards(string name)
+	{
+		rewards.push_back(name);
+	}
+
+};
+
+#pragma endregion
+
+
 int main()
 {
-	GameGenre gameGenre("Read Dead Redemption");
-	gameGenre.GameSoldCopies.push_back("I am back");
-	gameGenre.GameDeveloper = "Rockstar North";
-	gameGenre.GameCopies = 1000;
-	gameGenre.GameSoldCopies = { "Hundered","Million","Billion"};
-
-	cout << "Game Name: " << gameGenre.GameName << endl;
-	cout << "Number of copies sold" << endl;
-	for (string title : gameGenre.GameSoldCopies)
-	{
-		cout << title<<endl;
-	}
+	PlayerData player1("Bean", 10, 2);
+	player1.AddRewards("Brave Solider");
+	player1.AddRewards("Unbeatable");
+	
+	player1.GetInfo();
 
 	cin.get();
 }
