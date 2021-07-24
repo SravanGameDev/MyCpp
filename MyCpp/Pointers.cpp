bb32089c9a3b7a303876c5fe56a5f6ab5f1160bf
Sravan Kumar K <sravan.kumar1691@gmail.com>
2021-07-24 14:02:21 +0530

    Pointers
    Question
    Returning multiple values from a function

--- MyCpp/Pointers.cpp
+++ MyCpp/Pointers.cpp
@@ -102,11 +102,62 @@ void OutputArraysWithPointers()
 
 #pragma region Return Multiple Values from a function
 
-#pragma endregion
+int getMin(int numbers[], int size)
+{
+	int min = numbers[0];
+	for (size_t i = 1; i < size; i++)
+	{
+		if (numbers[i] < min)
+			min = numbers[i];
+	}
 
+	return min;
+}
+
+int getMax(int numbers[], int size)
+{
+	int max = numbers[0];
+	for (size_t i = 1; i < size; i++)
+	{
+		if (numbers[i] > max)
+			max = numbers[i];
+	}
+
+	return max;
+}
+
+void getMinMax(int numbers[], int size, int* min, int* max)
+{
+	for (size_t i = 1; i < size; i++)
+	{
+		if (numbers[i] < *min)
+			*min = numbers[i];
+		if (numbers[i] > *max)
+			*max = numbers[i];
+	}
+}
+
+
+void OutputMultipleValues()
+{
+	int numbers[5] = { 5,4,-13,52,65 };
+
+	cout << "Min is " << getMin(numbers, 5) << endl;
+	cout << "Max is " << getMax(numbers, 5) << endl;
+
+	int min = numbers[0];
+	int max = numbers[0];
+
+	cout << "\nUsing Pointers" << endl;
+	getMinMax(numbers, 5, &min, &max);
+	cout << "Min is " << min << endl;
+	cout << "Max is " << max << endl;
+}
+#pragma endregion
 
 int main()
 {
-	
+	OutputMultipleValues();
+
 	cin.get();
 }
