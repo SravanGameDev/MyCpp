af14a295073f936e014215cbc14008bddfacb433
Sravan Kumar K <sravan.kumar1691@gmail.com>
2021-07-29 09:28:46 +0530

    Local Static
    Question:
    Using static inside a function and incrementing a value

--- MyCpp/StaticInstances.cpp
+++ MyCpp/StaticInstances.cpp
@@ -1,12 +1,15 @@
 #include<iostream>
 
-/* Notes:
+/* Topic Notes: Static
 * it is to a specify a private in a cpp file.
 * it create only a instance, if there are different instances of class.
 * it stores the information which can be shared across all class instances.
 * static method can only access the static variable inside the class.
 * instead of a global variable, static global variable is helpful.
+* Examples: score, lives
 */
+
+#pragma region Static
 
 // I used struct because the members function and variable are default public
 struct Entity
@@ -23,7 +26,7 @@ struct Entity
 int Entity::x;
 int Entity::y;
 
-int  main()
+void  OutputStatic()
 {
 	Entity e;
 	e.x = 2;
@@ -34,7 +37,47 @@ int  main()
 
 	e.Print();
 	Entity::Print();
+}
+
+#pragma endregion
+
+#pragma region Local Static
+
+void function(char stat)
+{
+	/* Single instance is created so the value of i is stored to increment */
+	if (stat == 's')
+	{
+		static int i = 0;
+		i++;
+		std::cout<<"Static value: " << i << std::endl;
+	}
+	else
+	{
+		int i = 0;
+		i++;
+		std::cout << "Value: " << i << std::endl;
+	}
+}
+
+
+void OutputLocalStatic()
+{
+	int length = 10;
+	/* Value increase based on previous data which is an instance for all functions */
+	for (size_t i = 0; i < length; i++)
+		function('s');
+
+	/* It creates a new every time whenever it exists out of the scope */
+	for (size_t i = 0; i < length; i++)
+		function(NULL);
+}
+
+#pragma endregion
+
+int main()
+{
+	OutputLocalStatic();
 
 	std::cin.get();
 }
-
